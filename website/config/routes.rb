ff22131dc1d9ee020fac065728b8adc3eac324ee
Rawan Kasem <roropunkii@gmail.com>
2017-08-22 10:29:21 +0200

    =added main controller and pages inside it.

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'main/gallery_2'
+
+  get 'main/robotics'
+
+  get 'main/index'
+
+  get 'main/gallery'
+
+    get 'main/index'
+    root 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
