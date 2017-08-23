c22733b978c40db4850db321a5217915203aaf6e
Rawan Kasem <roropunkii@gmail.com>
2017-08-23 14:04:59 +0200

    =server worked for index

--- website/app/controllers/main_controller.rb
+++ website/app/controllers/main_controller.rb
@@ -1,15 +1,15 @@
 class MainController < ApplicationController
     def index
-        redirect_to 'index.html.erb'
+
     end
     def gallery 
-        redirect_to 'gallery.html.erb'
+
     end
     def gallery_2 
-        redirect_to 'gallery_2.html.erb'
+
     end
     def robotics 
-        redirect_to 'robotics.html.erb'
+
     end
     
 end
