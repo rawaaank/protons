461981ab2e885b668601a901d103f25ee241fda4
Rawan Kasem <roropunkii@gmail.com>
2017-08-23 08:47:34 +0200

    =completed changing the <a> tags to link_to

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   get 'main/index'
 
   get 'main/gallery'
-
-    get 'main/index'
+    
     root 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
