7d6593c8c6f18f4fc174f54c07097f80936bdc07
sakurashahd <husseinshahd1@gmail.com>
2017-08-24 23:02:21 +0200

    posts users

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,19 @@
+class MainController < ApplicationController
+    def index
+
+    end
+    def gallery 
+
+    end
+    def gallery_2 
+
+    end
+    def robotics 
+
+    end
+
+   def joinus
+    render :layout => false
+   end
+    
+end
---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  validates_presence_of :title
+  validates_presence_of :content
+
+end
